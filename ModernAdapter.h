d41f6f760040a792124d122348be7eda988d88b7
superbunny38 <superbunny38@gmail.com>
2026-01-30 15:06:19 -0500

    skeleton

--- ModernAdapter.h
+++ ModernAdapter.h
@@ -4,3 +4,23 @@
 #include <vector>   
 #include <memory>
 
+using namespace std;
+
+struct ModernDoc
+{
+    string title;
+    string body;
+    int id;
+};
+
+class ModernAdapter
+{
+    private:
+    // a place to store the data we convert
+
+    public:
+    //constructor
+        ModernAdapter() = default;
+
+        void Run();
+};
