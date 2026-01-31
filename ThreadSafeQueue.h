301a1786844de79b6de66296b41eabaa2c37b1b2
superbunny38 <superbunny38@gmail.com>
2026-01-31 16:42:58 -0500

    make public

--- ThreadSafeQueue.h
+++ ThreadSafeQueue.h
@@ -8,5 +8,19 @@ using namespace std;
 
 template <typename T>
 class ThreadSafeQueue {
+    private:
+        queue<T> Q;
+        mutable mutex Mutex;
+        condition_variable Cv;
     
+    public:
+        void push(T value){
+            {lock_guard<mutex> lock(Mutex);
+            q.push(move(value));}
+            Cv.notify_one();
+        }
+
+        bool pop(T& result){
+
+        }
 };
