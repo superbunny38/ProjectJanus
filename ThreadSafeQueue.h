7a0f2755892682fa0ff854768e4f225d0df54fe1
superbunny38 <superbunny38@gmail.com>
2026-01-31 16:38:41 -0500

    ThreadSafeQueue

---
+++ ThreadSafeQueue.h
@@ -0,0 +1,12 @@
+#include <queue>
+#include <mutex>
+#include <thread> 
+#include <chrono>
+#include <condition_variable>
+
+using namespace std;
+
+template <typename T>
+class ThreadSafeQueue {
+    
+};
