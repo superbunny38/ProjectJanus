ab7abf18d73e4986d2dd27419a47bba815dc20ea
superbunny38 <superbunny38@gmail.com>
2026-02-01 17:51:59 -0500

    Phase 3 begins

--- main.cpp
+++ main.cpp
@@ -11,6 +11,13 @@ int main() {
     ThreadSafeQueue<shared_ptr<ModernDoc>> documentQueue;//
     ModernAdapter adapter(documentQueue);
 
+    /*
+    thread: This launches a new timeline. Everything inside the { ... } runs in parallel with the rest of your main function.
+
+    [&]: This is a "Capture List." It gives the thread permission to access variables from main (like adapter and documentQueue) by reference.
+
+    Why shutdown()? This is crucial. When adapter.Run() finishes (after 20 items), the thread needs to yell "STOP!" If it didn't, the consumer would sit at the empty conveyor belt waiting forever.
+    */
     thread producerThread([&]() {
         adapter.Run();//Do the work
         documentQueue.shutdown(); // Signal that production is done
