908e795045f1c17739e9c9ef9462fa171a2deaa0
superbunny38 <superbunny38@gmail.com>
2026-02-02 13:07:22 -0500

    parallelim + synch + safety

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ int main() {
     ModernAdapter adapter(documentQueue);
     InvertedIndex index;
 
+    cout << "---Search Indexer Started---" << endl;
+
     /*
     thread: This launches a new timeline. Everything inside the { ... } runs in parallel with the rest of your main function.
 
@@ -20,18 +22,33 @@ int main() {
 
     Why shutdown()? This is crucial. When adapter.Run() finishes (after 20 items), the thread needs to yell "STOP!" If it didn't, the consumer would sit at the empty conveyor belt waiting forever.
     */
-    thread producerThread([&]() {
-        adapter.Run();//Do the work
-        documentQueue.shutdown(); // Signal that production is done
-    });
+    {
+        thread producerThread([&]() {
+            adapter.Run();//Do the work
+            documentQueue.shutdown(); // Signal that production is done
+        });
 
-    shared_ptr<ModernDoc> doc;
-    cout << "---Search Indexer Started---" << endl;
-    while (documentQueue.pop(doc)) {
-        cout << "Indexed Document ID: " << doc->id << ", Title: " << doc->title << endl;
+        
+        
+        jthread consumerThread([&](){
+            shared_ptr<ModernDoc> doc;
+            while (documentQueue.pop(doc)) {
+                index.addDocument(doc->id, doc->title + " " + doc->body);
+            }
+        });
+
+        producerThread.join();
     }
 
-    producerThread.join();
+    cout <<"---Performing Search for 'cloud'---" << endl;
+    auto results = index.search("cloud");
+
+    for (const auto& [docId, frequency] : results) {
+        cout << "Document ID: " << docId << ", Frequency: " << frequency << endl;
+    }
+
     cout << "---Search Indexer Stopped---" << endl;
+
+    
     return 0;
 }
